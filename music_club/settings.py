ee3bc97051f9500ab747e0c8c41deaf9ca937ab1
Samuel Raj <samchenu13@gmail.com>
2026-02-13 20:32:48 +0530

    Configure SendGrid SMTP properly

--- music_club/settings.py
+++ music_club/settings.py
@@ -171,8 +171,11 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'samchenu13@gmail.com'        # <-- Your Gmail
-EMAIL_HOST_PASSWORD = 'owmuryzoivsewdpz'  # <-- 16-digit App Password (no spaces)
+##EMAIL_HOST_USER = 'samchenu13@gmail.com'        # <-- Your Gmail
+##EMAIL_HOST_PASSWORD = 'owmuryzoivsewdpz'  # <-- 16-digit App Password (no spaces)
+
+EMAIL_HOST_USER = "apikey"
+EMAIL_HOST_PASSWORD = os.environ.get("SENDGRID_API_KEY")
 DEFAULT_FROM_EMAIL = "samchenu13@gmail.com"
 
 UPI_ID = upi_id
