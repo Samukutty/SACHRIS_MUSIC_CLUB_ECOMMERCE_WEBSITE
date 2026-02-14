f55bfdcb97361c359f28881900d580b71d958d3f
Samuel Raj <samchenu13@gmail.com>
2026-02-14 12:44:23 +0530

    Fix SendGrid SMTP host

--- music_club/settings.py
+++ music_club/settings.py
@@ -166,8 +166,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'uploads'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST = "smtp.sendgrid.net"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
